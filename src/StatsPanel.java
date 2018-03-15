fa82813b223adfacae66e3e22f53448fb2e6d3ac
Danilo Del Busso <k1763928@kcl.ac.uk>
2018-03-15 12:01:56 +0000

    Modified imports; Fixed and updated comments; Added parameters as fields for future possible use; Modified the initialiseSubPanels() method so it takes the number of panels to initialise as parameter; Modified the initialiseCentralPanels() method so it takes a List<AppPanel> as parameter; Organised order of methods so they follow an intuitive logic; getFreeCentralPanelName() doesn't exist anymore, substituted with:  * getFreePreviousCentralPanel(AppPanel currentPanel) which returns the previous central panel that is not visible  * getFreeNextCentralPanel(AppPanel currentPanel) which returns the next central panel that is not visible ; substituted the getCentralPanels() method with returnNewCentralPanel(); created the centralPanelIsVisible(AppPanel panelToCheck) which returns true if the panelToCheck is currently visible on screen.
    Overall, changed the structure of the StatsPanel class so it's more generalised and organised. The central panels are now stored in this class
    and if a StatsSubPanel needs a new central panel it calls methods in the StatsPanel.

--- src/StatsPanel.java
+++ src/StatsPanel.java
@@ -1,16 +1,11 @@
-import com.sun.deploy.panel.JavaPanel;
-
-import javax.swing.*;
 import java.awt.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
+import java.util.List;
 
-
-/** The statistics panel is based on CardLayout and allows for different panels which show different
-    * statistics about the application and its data.
-    * @author Danilo Del Busso
+/**A Stats Panel has a number of subpanels in GridLayout that
+    * show individual central panels with statistics about the listings in
+    * the main application.
+    * @author Danilo Del Busso, Luka Kralj
     * @version 14.03.2018
     */
 public class StatsPanel extends AppPanel {
@@ -19,111 +14,162 @@ public class StatsPanel extends AppPanel {
        * we are expecting one specialised panel to be created for each
        * needed section
        */
-    private static ArrayList<StatsCentralPanel> centralPanels = new ArrayList<>();
-    private static ArrayList<StatsCentralPanel> centralPanelsInUse = new ArrayList<>();
+    private ArrayList<AirbnbListing> listings;
+    private int lowPrice;
+    private int highPrice;
+    private ArrayList<AppPanel> centralPanels;
     private ArrayList<StatsSubPanel> statsSubPanels;
-    //there needs be 4 additional statistics panels that
-        // are not created for possible coupling reasons
 
     /**
-     * Create a new statistics panel to be displayed in the main window.
-     * @param listings
-     * @param lowPrice
-     * @param highPrice
+     * Initialise variables, create subpanels and give them initial central panels
+     * to show initial statistics.
+     * @param statsPanels the central panels which show the stats
+     * @param listings the list of all listings in the application
+     * @param lowPrice the lower bound for price range search
+     * @param highPrice the upper bound for price range search
+     * @throws Exception
      */
-    public StatsPanel(ArrayList<AirbnbListing> listings, int lowPrice, int highPrice) {
+    public StatsPanel(List<AppPanel> statsPanels, ArrayList<AirbnbListing> listings, int lowPrice, int highPrice, int numberOfSubPanels) throws Exception {
         super("Statistics", listings, lowPrice, highPrice);
 
-        //set the layout to be GridLayout, 2 rows and 2 columns
-        setSize(200,200);
+        this.listings = listings;
+        this.lowPrice = lowPrice;
+        this.highPrice = highPrice;
+        //set the layout to be GridLayout, 2 rows and 2 columns with a spacing of 10 pixels between each subpanel
         setLayout(new GridLayout(2,2,10,10));
-
-            //initialise all the statistics panels (8 in this case)
-        centralPanels = new ArrayList<StatsCentralPanel>();
+        //initialise data structures containing panels shown
+        centralPanels = new ArrayList<AppPanel>();
         statsSubPanels = new ArrayList<StatsSubPanel>();
-
-        initialiseCentralPanels();
-        initialiseSubPanels();
-
+        //initialise and add visual elements
+        initialiseCentralPanels(statsPanels);
+        initialiseSubPanels(numberOfSubPanels); //"containers" for central panels
         setVisible(true);
     }
 
-
+    /**
+     * Inserts the panels that contain different statistics in the StatsPanel
+     */
+    public void initialiseCentralPanels(List<AppPanel> centralPanelsToAdd){
+        for(AppPanel centralPanelToAdd: centralPanelsToAdd){
+            centralPanels.add(centralPanelToAdd);
+        }
+    }
 
     /**
-     * Create Instances of the StatsSubPanles and add
+     * Create Instances of the StatsSubPanels and add
      * them to the statisticsPanels so they can be displayed
+     * @param numberOfPanels the number of "containers" (StatsSubPanel) that are shown at any time on screen
      */
-    private void initialiseSubPanels() {
-
-
-        StatsSubPanel sb1 = new StatsSubPanel(this);
-        statsSubPanels.add(sb1);
-        StatsSubPanel sb2 = new StatsSubPanel(this);
-        statsSubPanels.add(sb2);
-        StatsSubPanel sb3 = new StatsSubPanel(this);
-        statsSubPanels.add(sb3);
-        StatsSubPanel sb4 = new StatsSubPanel(this);
-
-        statsSubPanels.add(sb4);
-
-        for(StatsSubPanel subPanel:statsSubPanels){
+    private void initialiseSubPanels(int numberOfPanels) throws Exception {
+        //check if the number of central panels is bigger than the expected number of sub panels that have
+        // to contain them (pigeon hole principle)
+        if(numberOfPanels > centralPanels.size()){
+            throw new Exception("There are too few central panels and/or too many subpanels");
+        }
+        for(int i = 0; i<numberOfPanels;i++){
+            statsSubPanels.add(new StatsSubPanel(this));
+        }
+        //add statssubpanels to this statspanel
+        for(StatsSubPanel subPanel : statsSubPanels){
             add(subPanel);
         }
 
     }
 
-    public ArrayList<StatsCentralPanel> getCentralPanels() {
-        return centralPanels;
-    }
-
-    public  void initialiseCentralPanels() {
-
-        CentralPanel1 c1 = new CentralPanel1();
-        CentralPanel2 c2 = new CentralPanel2();
-        CentralPanel3 c3 = new CentralPanel3();
-        CentralPanel4 c4 = new CentralPanel4();
-        CentralPanel5 c5 = new CentralPanel5();
-
-
-        centralPanels.add(c1);
-        centralPanels.add(c2);
-        centralPanels.add(c3);
-        centralPanels.add(c4);
-        centralPanels.add(c5);
-
-
-    }
-
-    public ArrayList<StatsSubPanel> getStatsSubPanels() {
-        return statsSubPanels;
-    }
-
-
     /**
-     * Return the name of a card that is not currently displayed by any subpanel
-     * @return the name of a card that is not currently displayed by any subpanel
+     * Return the next central stats panel that is not visible on screen.
+     * If all central panels are visible return null.
+     * @param currentPanel The panel currently shown that we are substituting. If null there's no panel.If null there's no panel.
+     * @return The next central stats panel that is not visible on screen.
      */
-    public String getFreeCentralPanelName(){
-        String name = null;
-
-        for(StatsCentralPanel centralPanel : centralPanels){
-            boolean found = false;
-           if(! (statsSubPanels.size()<5)){
-            for(StatsSubPanel subPanel: statsSubPanels){
-                System.out.println(subPanel.getCurrentCardName());
-                if(centralPanel.getName().equals(subPanel.getCurrentCardName())){
-                   found = true;
+    public AppPanel getFreeNextCentralPanel(AppPanel currentPanel) {
+        if(currentPanel != null){
+            int indexOfCurrentPanel = centralPanels.indexOf(currentPanel);
+            for(int i = 0; i < centralPanels.size(); i++){
+                if(indexOfCurrentPanel == centralPanels.size()-1){
+                    indexOfCurrentPanel = -1;
+                }
+                indexOfCurrentPanel++;
+                AppPanel newPanel = centralPanels.get(indexOfCurrentPanel);
+                if (!centralPanelIsVisible(newPanel)){
+                    return newPanel;
                 }
             }
-           }
-            //if it not found in current cards in the sub panels, it is not used
-            if(!found){
-                return centralPanel.getName();
-            }
-
         }
-        return name;
-    };
+        //if currentpanel is null, we assume there's no panel and we return one random panel that is not currently shown
+        else{
+            return returnNewCentralPanel();
+        }
+        return null;
+    }
 
+    /**
+     * Return the previous central stats panel that is not visible on screen.
+     * If all central panels are visible return null.
+     * @param currentPanel The panel currently shown that we are substituting. If null there's no panel.
+     * @return The previous central stats panel that is not visible on screen
+     */
+    public AppPanel getFreePreviousCentralPanel(AppPanel currentPanel) {
+
+        //
+        if(currentPanel != null){
+            int indexOfCurrentPanel = centralPanels.indexOf(currentPanel);
+            for(int i = 0; i < centralPanels.size(); i++){
+                if(indexOfCurrentPanel == 0){
+                    indexOfCurrentPanel = centralPanels.size();
+                }
+                indexOfCurrentPanel--;
+                AppPanel newPanel = centralPanels.get(indexOfCurrentPanel);
+                if (!centralPanelIsVisible(newPanel)){
+                    return newPanel;
+                }
+            }
+        }
+        //if currentpanel is null, we assume there's no panel and we return one random panel that is not currently shown
+        else{
+            return returnNewCentralPanel();
+        }
+        return null;
+    }
+
+    /**
+     * Returns the first central panel in the centralPanels arraylist that is not
+     * currently visible on screen.
+     * @return The first central panel in the centralPanels arraylist that is not
+     * currently visible on screen.
+     */
+    private AppPanel returnNewCentralPanel() {
+        if(statsSubPanels.size() == 0){
+            //if no subpanel has been created then there's no central panel that is currently visible
+            return centralPanels.get(0);
+        }
+        for(AppPanel panel : centralPanels){
+            if(!centralPanelIsVisible(panel)){
+                return panel;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Return true if the centralPanel is currently visible on screen.
+     * @param panelToCheck the central panel we're looking for on screen
+     * @return true if the centralPanel is currently visible on screen.
+     */
+    private boolean centralPanelIsVisible(AppPanel panelToCheck) {
+        if(!statsSubPanels.isEmpty()){
+            for(StatsSubPanel subPanel: statsSubPanels){
+                String panelToCheckName = panelToCheck.getTitle();
+                AppPanel subPanelCentralPanel = (AppPanel) subPanel.getCentralPanel();
+                String subPanelCentralPanelName = subPanelCentralPanel.getTitle();
+                //if it is found as centralPanel of a subPanel then it is visible
+                if(panelToCheckName.equals(subPanelCentralPanelName)){
+                    return true;
+                }
+            }
+        }
+        //if it is not found as centralPanel of a subPanel then it not is visible
+        return false;
+
+    }
 }
