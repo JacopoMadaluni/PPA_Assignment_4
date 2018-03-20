b618a754f0cf5f0242dba5194d421d420316e3db
Danilo Del Busso <k1763928@kcl.ac.uk>
2018-03-20 19:11:14 +0000

    Modified AvgReviewScore so that it now displays a bar chart created using the JFreeChart library. Added a method to create the dataset. All values shown on screen are now 0 because of dataset conflict (no data given for review scores in dataset).

--- src/AvgReviewScore.java
+++ src/AvgReviewScore.java
@@ -1,8 +1,13 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AvgReviewScore extends AppPanel {
-    ArrayList<AirbnbListing> listingsInRange;
     int lowPrice;
     int highPrice;
 
@@ -12,13 +17,74 @@ public class AvgReviewScore extends AppPanel {
      * @param lowPrice  Lower price boundary of the properties the user wants to see.
      * @param highPrice Upper price boundary of the properties the user wants to see.
      */
-    public AvgReviewScore( List<AirbnbListing> listings, int lowPrice, int highPrice) {
-        super("Average Review Score", listings, lowPrice, highPrice);
+    public AvgReviewScore(String title, List<AirbnbListing> listings, int lowPrice, int highPrice) {
+        super(title, listings, lowPrice, highPrice);
         this.lowPrice = lowPrice;
         this.highPrice = highPrice;
-        listingsInRange = new ArrayList<>();
-        getListingsInRange(listings);
+        double avg = calculateAvgReviewScore(listings, lowPrice, highPrice);
 
+        JFreeChart barChart = ChartFactory.createBarChart(
+                title,
+                "Review Range by Listing Price",
+                "Number of Reviews",
+                createDataset(),
+                PlotOrientation.VERTICAL,
+                true, true, false);
+
+        ChartPanel chartPanel = new ChartPanel( barChart );
+        chartPanel.setPreferredSize(new java.awt.Dimension( 500 , 400) );
+        add( chartPanel );
+
+        setVisible(true);
+
+
+
+    }
+
+    /**
+     * Create the dataset for the data visualisation
+     * @return the dataset for the data visualisation
+     */
+    private CategoryDataset createDataset( ) {
+
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+
+        //categories
+        final String low =  "0£ to "+ highPrice/4 +"£";
+        final String mediumLow = highPrice/4+"£" + " to " +(highPrice/4 *2)+"£";
+        final String mediumHigh =highPrice/4*2+"£" + " to " +(highPrice/4 *3)+"£";
+        final String high = highPrice/4 *3+"£" + " to " +highPrice+"£";
+        final String all = "Total Average";
+
+        //name of group of bar graphs
+        final String averageValue = "All listings";
+
+        // add values to dataset
+        dataset.addValue(calculateAvgReviewScore(listings, lowPrice,highPrice/4), low, averageValue );
+        dataset.addValue(calculateAvgReviewScore(listings, highPrice/4,highPrice/4*2), mediumLow, averageValue );
+        dataset.addValue(calculateAvgReviewScore(listings, highPrice/4*2,highPrice/4*3), mediumHigh, averageValue );
+        dataset.addValue(calculateAvgReviewScore(listings, highPrice/4*3,highPrice/4), high, averageValue );
+        dataset.addValue( calculateAvgReviewScore(listings, lowPrice,highPrice), all , averageValue );
+
+        return dataset;
+    }
+
+    /**
+     * Return the average review score of the listings given as parameter
+     * @param listings the dataset for the calculation
+     * @return the average review score of the listings given as parameter
+     */
+    private double calculateAvgReviewScore(List<AirbnbListing> listings, int lowBound ,int highBound) {
+        double sum = 0;
+        double avg = 0;
+        for(AirbnbListing listing: listings){
+            if(listing.getPrice()<=highBound && listing.getPrice()>=lowBound){
+                //  TODO
+                //can only be done after requirement conflict has been resolve (no data in dataset)
+            }
+
+        }
+        return avg;
     }
 
 
@@ -27,13 +93,13 @@ public class AvgReviewScore extends AppPanel {
      * listingsInRange arrayList
      * @param listings the entirety of all listings in the dataset
      */
-    private void getListingsInRange(List<AirbnbListing> listings) {
+    private ArrayList<AirbnbListing> getListingsInRange(List<AirbnbListing> listings) {
+        ArrayList<AirbnbListing> listingsInRange = new ArrayList<>();
         for(AirbnbListing listing: listings){
             if(listing.getPrice() <= highPrice && listing.getPrice()>= lowPrice){
                 listingsInRange.add(listing);
             }
         }
-
-
+        return listingsInRange;
     }
 }
