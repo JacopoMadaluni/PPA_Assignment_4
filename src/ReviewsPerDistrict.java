a39487e0a89b167e2bf7f0683a728112d5f64ce0
AlvaroRausell <alvaro.rausellg@gmail.com>
2018-03-22 11:37:25 +0000

    Comments added

--- src/ReviewsPerDistrict.java
+++ src/ReviewsPerDistrict.java
@@ -6,11 +6,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class creates a bar chart representing the
+ * average number of reviews per district. It extends the
+ * BarChart class to create the graph.
+ * @author Alvaro Rausell
+ * @version 22.03.2018*/
 public class ReviewsPerDistrict extends BarChart {
 
     //Selects te number of neighborhoods to display
     private static final int topDistricts = 10;
+    //Collection of all districts with their average number of reviews
     private HashMap<String,Double> reviewsPerDistrict;
+
+    /**
+     * Creates the ReviewsPerDistrict object by calling the constructor
+     * from BarChart.
+     * @param title Title of the graph
+     * @param listings Collection of Airbnb's from which to take the data
+     * @param lowPrice Minimum price that the properties may cost
+     * @param highPrice Maximum prices the the properties may cost*/
     public ReviewsPerDistrict(String title, ArrayList<AirbnbListing> listings, int lowPrice, int highPrice){
         super(title,listings,lowPrice,highPrice);
     }
@@ -29,6 +44,12 @@ public class ReviewsPerDistrict extends BarChart {
         return null;
     }
 
+    /**
+     * Creates the set of data for the graph.
+     * @param listings List of Airbnb's
+     * @param lowPrice Lowest price bound
+     * @param highPrice Highest price bound
+     * @return Dataset for the graph*/
     @Override
     protected Dataset createDataset(List<AirbnbListing> listings, int lowPrice, int highPrice) {
         reviewsPerDistrict = new HashMap<>();
@@ -44,12 +65,22 @@ public class ReviewsPerDistrict extends BarChart {
         return dataset;
     }
 
+    /**
+     * Gets the average number of reviews given an index.
+     * This index represents the order by number of reviews.
+     * @param i position in the number of reviews scale
+     * @return Average number of reviews at the given position*/
     private double getReviewAvg(int i){
         ArrayList<Double> values = new ArrayList<>(reviewsPerDistrict.values());
         Collections.sort(values);
         Collections.reverse(values);
         return values.get(i-1);
     }
+    /**
+     * Gets the name of the District based on an index.
+     * This index represents the order by number of reviews.
+     * @param i position in the number of reviews scale
+     * @return Name of the district at the given position*/
     private String getDistrictName(int i){
         ArrayList<Double> values = new ArrayList<>(reviewsPerDistrict.values());
         Collections.sort(values);
@@ -65,7 +96,10 @@ public class ReviewsPerDistrict extends BarChart {
     public String getYLabel() {
         return "Average Number of Reviews";
     }
-
+    /**
+     * Sets up the collection of district names and average number of reviews
+     * @param lowPrice Lower price bound
+     * @param highPrice Upper price bound*/
     private void setDistricts(int lowPrice, int highPrice){
         reviewsPerDistrict.put(
                 "Kingston upon Thames",
@@ -136,6 +170,14 @@ public class ReviewsPerDistrict extends BarChart {
                  getAvgReviewsPerDistrict(listings, lowPrice, highPrice, "Barking and Dagenham"));
     }
 
+    /**
+     * Gets the average number of reviews for a given district
+     * @param listings List of Airbnb's
+     * @param lowPrice Lower price bound
+     * @param highPrice Upper price bound
+     * @param districtName Name of the district
+     * @return Average number of reviews of the given district
+     */
     private double getAvgReviewsPerDistrict(List<AirbnbListing> listings, int lowPrice, int highPrice, String districtName){
         int sum = 0;
         int avg;
