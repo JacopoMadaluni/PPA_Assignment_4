e4b158a583f9ff78a3fec4a0eb0d53159096423c
Alvaro Rausell <k1763934@kcl.ac.uk>
2018-03-25 20:46:46 +0100

    Created Comparator

--- src/Comparator.java
+++ src/Comparator.java
@@ -1,3 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Comparator {
+    private JFrame frame;
+
+    public Comparator(){
+        frame = new JFrame("AirBnb Comparator");
+        Container container = frame.getContentPane();
+        JLabel status = new JLabel(Main.getMyList().isEmpty()? "No properties in My List, please add them in the" +
+                "single property view":"You have "+Main.getMyList().size()+" properties");
+        JButton viewList = new JButton("View List");
+        viewList.setEnabled(!Main.getMyList().isEmpty());
+        viewList.addActionListener((e)->{
+            BnbTable table = new BnbTable(Main.getMyList());
+            table.displayBnbList();
+        });
+    }
+
 
 }
