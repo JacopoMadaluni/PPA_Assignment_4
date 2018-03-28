8c2da119b36fafffd5fb8efdc7c352a44440b0c1
AlvaroRausell <alvaro.rausellg@gmail.com>
2018-03-28 19:04:58 +0100

    Layout set for Comparator.java

--- src/PropertyFinder/Comparator.java
+++ src/PropertyFinder/Comparator.java
@@ -8,8 +8,9 @@ public class Comparator extends AppPanel {
     public Comparator(MainWindow mainWindow){
         super("PropertyFinder.Comparator",MainWindow.getMyList(),0,0);
         listings = mainWindow.getMyList();
-        JLabel status = new JLabel(listings.isEmpty()? "No properties in My List, please add them in the" +
-                " single property view or, if you have done so already, press the 'refresh button'":"You have "+listings.size()+(listings.size() > 1?" properties":" property"));
+        JTextArea status = new JTextArea(listings.isEmpty()? "No properties in My List, please add them in the" +
+                " single property view.\nIf you have done so already, press the 'refresh button'":"You have "+listings.size()+(listings.size() > 1?" properties":" property"));
+        status.setFont(new Font("Arial",Font.PLAIN,18));
         JButton refresh = new JButton("Refresh");
         refresh.addActionListener((e)->{
             System.out.println("Done");
@@ -27,9 +28,11 @@ public class Comparator extends AppPanel {
             }
 
         });
+        setBackground(Color.WHITE);
         setLayout(new BorderLayout());
         compare.setSize(100,25);
         JPanel buttons = new JPanel();
+        buttons.setBackground(Color.WHITE);
         buttons.add(compare);
         buttons.add(refresh);
         add(buttons,BorderLayout.AFTER_LAST_LINE);
