bf6bc666bbdf366bc5b680e9355a3cb38a14246f
Alvaro Rausell <k1763934@kcl.ac.uk>
2018-03-29 15:13:29 +0100

    Comments added and setPage() method created

--- src/PropertyFinder/Comparator.java
+++ src/PropertyFinder/Comparator.java
@@ -3,21 +3,43 @@ import PropertyFinder.Map.ComparatorTable;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Comparator class for the property finder.
+ * This will create a panel that shows a
+ * comparative table of the properties saved by the user.
+ *
+ * @author Alvaro Rausell
+ * @version 27/03/2018
+ */
 public class Comparator extends AppPanel {
-
+    private MainWindow mainWindow;
+    /**
+     *Creates the comparator panel.
+     * Requires a MainWindow to access MyList and refresh the page.
+     * @param mainWindow main window
+     */
     public Comparator(MainWindow mainWindow){
-        super("PropertyFinder.Comparator",MainWindow.getMyList(),0,0);
-        listings = mainWindow.getMyList();
+        //Creates the panel
+        super("Comparator",MainWindow.getMyList(),0,0);
+        this.mainWindow = mainWindow;
+        this.listings = mainWindow.getMyList();
+        //Sets the elements of the page
+        this.setPage();
+    }
+    private void setPage(){
         JTextArea status = new JTextArea(listings.isEmpty()? "No properties in My List, please add them in the" +
                 " single property view.\nIf you have done so already, press the 'refresh button'":"You have "+listings.size()+(listings.size() > 1?" properties":" property"));
         status.setFont(new Font("Arial",Font.PLAIN,18));
         JButton refresh = new JButton("Refresh");
+        //Refreshes the page
         refresh.addActionListener((e)->{
             System.out.println("Done");
             mainWindow.highPriceClicked();
         });
         JButton compare = new JButton("Compare properties");
+        //Invisible while there are no properties in MyList
         compare.setVisible(!listings.isEmpty());
+        //Creates the comparison table
         compare.addActionListener((e)->{
             try {
                 JScrollPane comparison = new JScrollPane(new ComparatorTable(listings).makeTable());
@@ -26,8 +48,8 @@ public class Comparator extends AppPanel {
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
-
         });
+
         setBackground(Color.WHITE);
         setLayout(new BorderLayout());
         compare.setSize(100,25);
