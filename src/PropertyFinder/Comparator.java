fc0015c30da505c6a40ae77742cca7573c1fe2c8
Rausell, Alvaro (k1763934) <alvaro.rausell@kcl.ac.uk>
2018-03-29 17:28:30 +0100

    Compare button deleted

--- src/PropertyFinder/Comparator.java
+++ src/PropertyFinder/Comparator.java
@@ -28,7 +28,7 @@ public class Comparator extends AppPanel {
     }
     private void setPage(){
         JTextArea status = new JTextArea(listings.isEmpty()? "No properties in My List, please add them in the" +
-                " single property view.\nIf you have done so already, press the 'refresh button'":"You have "+listings.size()+(listings.size() > 1?" properties":" property"));
+                " single property view.\nIf you have done so already, press the 'refresh button'":"You have "+listings.size()+(listings.size() > 1?" properties":" property")+" in your list");
         status.setFont(new Font("Arial",Font.PLAIN,18));
         JButton refresh = new JButton("Refresh");
         //Refreshes the page
@@ -36,29 +36,20 @@ public class Comparator extends AppPanel {
             System.out.println("Done");
             mainWindow.highPriceClicked();
         });
-        JButton compare = new JButton("Compare properties");
-        //Invisible while there are no properties in MyList
-        compare.setVisible(!listings.isEmpty());
-        //Creates the comparison table
-        compare.addActionListener((e)->{
-            try {
-                JScrollPane comparison = new JScrollPane(new ComparatorTable(listings).makeTable());
-                add(comparison);
-                compare.setVisible(false);
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-        });
 
         setBackground(Color.WHITE);
         setLayout(new BorderLayout());
-        compare.setSize(100,25);
         JPanel buttons = new JPanel();
         buttons.setBackground(Color.WHITE);
-        buttons.add(compare);
         buttons.add(refresh);
         add(buttons,BorderLayout.AFTER_LAST_LINE);
         add(status,BorderLayout.PAGE_START);
+        if(!listings.isEmpty()){try {
+            JScrollPane comparison = new JScrollPane(new ComparatorTable(listings).makeTable());
+            add(comparison);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }}
     }
 
 
