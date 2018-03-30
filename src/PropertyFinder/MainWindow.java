7642dd5015b4a73df2ac66d742c796b49c9c55a1
lukakralj <luka.kralj2@gmail.com>
2018-03-30 01:37:35 +0100

    Added copyright button.

--- src/PropertyFinder/MainWindow.java
+++ src/PropertyFinder/MainWindow.java
@@ -215,6 +215,12 @@ public class MainWindow {
 
         bottom.add(left, BorderLayout.WEST);
         bottom.add(right, BorderLayout.EAST);
+
+        JPanel copyright = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        copyright.setOpaque(false);
+        copyright.add(createCopyright());
+        bottom.add(copyright, BorderLayout.CENTER);
+
         return bottom;
     }
 
@@ -387,5 +393,54 @@ public class MainWindow {
     public void disableButtons() {
         rightButton.setEnabled(false);
         leftButton.setEnabled(false);
+    }
+
+    /**
+     *
+     * @return A copyright button which displays info about the credits in a dialog window.
+     */
+    private JButton createCopyright() {
+        JButton copyButton = new JButton("\u00a9 The Order of the BlueJ");
+        copyButton.setForeground(Color.GRAY);
+        copyButton.setContentAreaFilled(false);
+        copyButton.setBorderPainted(false);
+        copyButton.setFocusable(false);
+        copyButton.addActionListener(e -> {
+            String text = "<html><div>" +
+                    "  <h3>ICONS</h3>" +
+                    "<ul>" +
+                    "<li><b>House</b> and <b>statistics</b> icons made by <a href=\"https://www.flaticon.com/authors/smashicons\" title=\"Smashicons\">Smashicons</a> </li>" +
+                    "<li><b>Pound</b> icons made by <a href=\"https://www.flaticon.com/authors/eleonor-wang\" title=\"Eleonor Wang\">Eleonor Wang</a>.</li>" +
+                    "<li><b>Rent house</b> icons made by <a href=\"https://www.iconfinder.com/icons/291025/building_construction_fence_home_house_property_real_estate_rent_renting_sevice_sign_icon#size=128\" title=\"Nicola Simpson\">Nicola Simpson</a>.</li>" +
+                    "<li><b>Moon</b>, <b>cityscape</b>, <b>budget</b>, <b>review</b> and <b>London Underground Sign</b> icons made by <a href=\"http://www.freepik.com\" title=\"Freepik\">Freepik</a>.</li>" +
+                    "</ul>" +
+                    "from <a href=\"https://www.flaticon.com/\" title=\"Flaticon\">www.flaticon.com</a> is licensed by <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\" target=\"_blank\">CC 3.0 BY</a>" +
+                    "<ul>" +
+                    "<li><b>AirBnB icon: </b> (AirBnB website <a href=\"https://www.airbnb.co.uk/\" title=\"airbnbIcon\">here</a>)</li>" +
+                    "</ul>" +
+                    "</div>" +
+                    "<div>" +
+                    "<h3>LONDON MAP</h3>" +
+                    "From <a href=\"https://commons.wikimedia.org/wiki/File:Greater_London,_administrative_divisions_-_de_-_colored.svg\" title=\"link\">Wikipedia Commons</a>, the free media repository made" +
+                    "by <a href=\"https://commons.wikimedia.org/wiki/User:TUBS\" title=\"TUBS\">TUBS</a>." +
+                    "</div>" +
+                    "<div>" +
+                    "<h3>LIBRARIES</h3>" +
+                    "<ul>" +
+                    "<li><b>Data Visualisation</b>: <a href=\"http://www.jfree.org/index.html\" title= \"JFreeChart\"> JFreechart </a></li>" +
+                    "</ul>" +
+                    "</div>" +
+                    "<div>" +
+                    "<h3>OTHER</h3>" +
+                    "<ul>" +
+                    "<li><b>Crime rates data</b>: from government records found <a href=\"https://data.london.gov.uk/dataset/recorded_crime_rates\" title= \"JFreeChart\">here</a></li>." +
+                    "</ul>" +
+                    "</div>" +
+                    "</html>";
+            JLabel label = new JLabel(text);
+            label.setFont(new Font("Ariel", Font.PLAIN, 15));
+            JOptionPane.showMessageDialog(null, label, "Copyright", JOptionPane.INFORMATION_MESSAGE);
+        });
+        return copyButton;
     }
 }
